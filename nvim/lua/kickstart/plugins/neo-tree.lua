0dd8cbe6a69964b79fb88f62e54194a920ffef14
Axel Blackert <axel.blackert@maxar.com>
2025-11-11 16:43:51 +0100

    Update neo-tree config

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -14,11 +14,36 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    -- Uncomment these lines if you want to disable icons entirely
+    -- default_component_configs = {
+    --   icon = {
+    --     folder_closed = "",
+    --     folder_open = "",
+    --     folder_empty = "",
+    --     default = "",
+    --   },
+    -- },
     filesystem = {
+      follow_current_file = {
+        enabled = true,          -- This will find and focus the file in the active buffer every time
+        leave_dirs_open = true,  -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['l'] = 'open',
+          ['h'] = 'close_node',
         },
+      },
+    },
+    event_handlers = {
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          -- Auto close neo-tree when a file is opened (optional)
+          -- Remove this if you want neo-tree to stay open
+          -- require("neo-tree.command").execute({ action = "close" })
+        end
       },
     },
   },
