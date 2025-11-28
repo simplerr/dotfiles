737269aef2aff771c1794193675deda8c923db22
Axel Blackert <axel.blackert@gmail.com>
2025-11-28 21:50:41 +0100

    Enable git blame on line as default

--- nvim_lazyvim/lua/config/keymaps.lua
+++ nvim_lazyvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
 vim.keymap.set("n", "<leader>ef", function()
   local explorer = Snacks.explorer.reveal()
   if explorer then
